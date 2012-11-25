8a1ef17c12698fafc2e838b6d971cebbedd13c3f
finscn <finscn@gmail.com>
2012-11-26 01:41:35 +0800

    update example

--- example/App/index.js
+++ example/App/index.js
@@ -11,28 +11,31 @@ ctx.font = "30px Arial bold";
 ctx.fillText("Pure WebView", 50, 200);
 ctx.fillText("Ejecta + WebView", 50, height / 2 + 200);
 
-var started=false;
+var started = false;
 document.addEventListener('touchend', function(ev) {
-                          if (started){ return;}
-                          started=true;
-    ctx.clearRect(0,0,width,height);
-	if(ev.touches[0].pageY < height / 2) {
-		runCase(true);
-	} else {
-		runCase(false);
-	}
-}, false);
+    console.log("touchend")
+    if(started) {
+        return;
+    }
+    started = true;
+    ctx.clearRect(0, 0, width, height);
+    if(ev.touches[0].pageY < height / 2) {
+        runCase(true);
+    } else {
+        runCase(false);
+    }
+}, true);
 
 
 function runCase(pureWebView) {
-	if(pureWebView) {
-		console.log("run : Pure WebView");
-		var webView = new Ejecta.WebView();
-		webView.src = "index.html";
-	} else {
-		console.log("run : Ejecta + WebView");
-		ejecta.require("pic-rain.js");
+    if(pureWebView) {
+        console.log("run : Pure WebView");
+        var webView = new Ejecta.WebView();
+        webView.src = "index.html";
+    } else {
+        console.log("run : Ejecta + WebView");
+        ejecta.require("pic-rain.js");
         ejecta.require("webview-hud.js");
-    
-	}
+
+    }
 }
--- example/App/pic-rain.js
+++ example/App/pic-rain.js
@@ -1,9 +1,8 @@
-
 window.devicePixelRatio = window.devicePixelRatio || 1;
 var Config = {
-	width: window.screen.availWidth * window.devicePixelRatio,
-	height: window.screen.availHeight * window.devicePixelRatio,
-	FPS: 60
+    width: window.screen.availWidth * window.devicePixelRatio,
+    height: window.screen.availHeight * window.devicePixelRatio,
+    FPS: 60
 }
 
 
@@ -18,198 +17,211 @@ var test;
 
 function init() {
 
-	test = new Demo({
-		canvas: "canvas",
-		sprites: createSprites()
-	});
-	test.init();
+    test = new Demo({
+        canvas: "canvas",
+        sprites: createSprites() || []
+    });
+    test.init();
+}
+
+function createStar(x, y) {
+    var w = 42,
+        h = 42
+    return new Block(x, y, w, h);
 }
 
 function createSprites() {
-	var list = [];
-	var row = 40,
-		col = 40;
-	var w=42, h=42;
-	var sw = Math.floor(Config.width / col),
-		sh = Math.floor(Config.height / row);
+    return;
+    
+    var list = [];
+    var row = 40,
+        col = 40;
+    var sw = Math.floor(Config.width / col),
+        sh = Math.floor(Config.height / row);
 
-	for(var r = 0; r < row; r++) {
-		for(var c = 0; c < col; c++) {
-			var b = new Block(sw * c, sh * r, w, h);
-			list.push(b);
-		}
-	}
+    for(var r = 0; r < row; r++) {
+        for(var c = 0; c < col; c++) {
+            var b = createStar(sw * c, sh * r);
+            list.push(b);
+        }
+    }
 
-	return list;
+    return list;
 }
 
 function Block(x, y, w, h) {
 
-	this.x = x;
-	this.y = y;
-	this.w = w;
-	this.h = h;
-	this.oy=y;
+    this.x = x;
+    this.y = y;
+    this.w = w;
+    this.h = h;
+    this.oy = y;
 
-	this.init();
+    this.init();
 }
 
 
 Block.prototype = {
 
-	init: function() {
-		this.x=this.x+random(-10, 10);
-		this.y=this.oy;
-		this.ax = 0;
-		this.ay = 0.00005;
-		this.vx = 0;
-		this.vy = random(0, 0.5);
-	},
-
-    reset : function(){
-        this.x=this.x+random(-10, 10);
-		this.y=-100;
-		this.ax = 0;
-		this.ay = 0.00005;
-		this.vx = 0;
-		this.vy = random(0.2, 0.5);
+    init: function() {
+        this.x = this.x + random(-10, 10);
+        this.y = this.oy;
+        this.ax = 0;
+        this.ay = 0.00005;
+        this.vx = 0;
+        this.vy = random(0, 0.5);
     },
-	update: function(timeStep) {
-        
-		if(this.y >Config.height+10) {
-			this.reset();
-		}
-		var vx = this.vx + this.ax * timeStep;
-		var vy = this.vy + this.ay * timeStep;
-		var dx = (this.vx + vx) / 2 * timeStep;
-		var dy = (this.vy + vy) / 2 * timeStep;
 
-		this.vx = vx;
-		this.vy = vy;
+    reset: function() {
+        this.x = this.x + random(-10, 10);
+        this.y = -100;
+        this.ax = 0;
+        this.ay = 0.00005;
+        this.vx = 0;
+        this.vy = random(0.2, 0.5);
+    },
+    update: function(timeStep) {
 
-		this.x += dx;
-		this.y += dy;
-	},
+        if(this.y > Config.height + 10) {
+            this.reset();
+        }
+        var vx = this.vx + this.ax * timeStep;
+        var vy = this.vy + this.ay * timeStep;
+        var dx = (this.vx + vx) / 2 * timeStep;
+        var dy = (this.vy + vy) / 2 * timeStep;
 
-	render: function(context, timeStep) {
-        context.drawImage(pic,this.x, this.y,this.w,this.h);
-	}
+        this.vx = vx;
+        this.vy = vy;
+
+        this.x += dx;
+        this.y += dy;
+    },
+
+    render: function(context, timeStep) {
+        context.drawImage(pic, this.x, this.y, this.w, this.h);
+    }
 }
 
 
 ///////////////////
 
 function Demo(cfg) {
-	for(var key in cfg) {
-		this[key] = cfg[key];
-	}
+    for(var key in cfg) {
+        this[key] = cfg[key];
+    }
 }
 
 Demo.prototype = {
 
-	canvas: null,
-	context: null,
-	sprites: null,
-	width: Config.width,
-	height: Config.height,
-	FPS: Config.FPS,
-	init: function() {
-		this.canvas = $id(this.canvas) || this.canvas;
-		this.canvas.width = this.width;
-		this.canvas.height = this.height;
-		this.context = this.canvas.getContext("2d");
-		this.context.font = "24pt Verdana";
-		this.context.fillStyle="#000000";
+    canvas: null,
+    context: null,
+    sprites: null,
+    width: Config.width,
+    height: Config.height,
+    FPS: Config.FPS,
+    init: function() {
+        this.canvas = $id(this.canvas) || this.canvas;
+        this.canvas.width = this.width;
+        this.canvas.height = this.height;
+        this.context = this.canvas.getContext("2d");
+        this.context.font = "24pt Verdana";
+        this.context.fillStyle = "#000000";
 
-		this.timeStep = 1000 / this.FPS;
-        this.maxTimeStep = this.timeStep*3;
-		var Me = this;
-		this.callRun = function() {
-			Me.run();
-		}
-		this.sprite = this.sprites || [];
+        this.timeStep = 1000 / this.FPS;
+        this.maxTimeStep = this.timeStep * 3;
+        var Me = this;
+        this.callRun = function() {
+            Me.run();
+        }
+        this.sprite = this.sprites || [];
 
-	},
-
-	start: function() {
-		this.avgTime = 1000 / this.FPS;
-		this.lastTime = Date.now() - this.avgTime;
-		this._count = 10;
-		this._fps = 0;
-		this.last = Date.now();
-		this.run();
-	},
-    pause : function(){
-        this.paused=true;
     },
-    resume : function(){
-        this.paused=false;
+
+    start: function() {
+        this.avgTime = 1000 / this.FPS;
+        this.lastTime = Date.now() - this.avgTime;
+        this._count = 10;
+        this._fps = 0;
+        this.last = Date.now();
+        this.run();
     },
-    paused : false,
-	blank: 0,
-	last: 0,
-	run: function() {
-		this.mainLoop = setTimeout(this.callRun, this.timeStep);
-		var now = Date.now();
-		var timeStep = this.blank + now - this.last;
-		this.last = now;
-        
-        if (!this.paused){
-             this.update(this.timeStep);
-            
-//            if (timeStep> this.maxTimeStep){
-//                timeStep= this.maxTimeStep;
-//            }
-//            while(timeStep >= this.timeStep) {
-//                this.update(this.timeStep);
-//                timeStep -= this.timeStep;
-//            }
-//            this.blank = timeStep;
+    pause: function() {
+        this.paused = true;
+    },
+    resume: function() {
+        this.paused = false;
+    },
+    paused: false,
+    blank: 0,
+    last: 0,
+    run: function() {
+        this.mainLoop = setTimeout(this.callRun, this.timeStep);
+        var now = Date.now();
+        var timeStep = this.blank + now - this.last;
+        this.last = now;
+
+        if(!this.paused) {
+            this.update(this.timeStep);
+
+            //            if (timeStep> this.maxTimeStep){
+            //                timeStep= this.maxTimeStep;
+            //            }
+            //            while(timeStep >= this.timeStep) {
+            //                this.update(this.timeStep);
+            //                timeStep -= this.timeStep;
+            //            }
+            //            this.blank = timeStep;
         }
 
-		this.render(this.context, timeStep);
+        this.render(this.context, timeStep);
 
-	},
+    },
 
-	update: function(timeStep) {
-		this.sprites.forEach(function(s) {
-			s.update(timeStep);
-		});
-	},
-	render: function(context, timeStep) {
-		context.fillRect(0,0,this.width,this.height);
-		this.sprites.forEach(function(s) {
-			s.render(context);
-		});
-	}
+    update: function(timeStep) {
+        this.sprites.forEach(function(s) {
+            s.update(timeStep);
+        });
+    },
+    render: function(context, timeStep) {
+        context.fillRect(0, 0, this.width, this.height);
+        this.sprites.forEach(function(s) {
+            s.render(context);
+        });
+    }
 
 }
 
 //////// utils ////////////
 
 function random(lower, higher) {
-	return(higher - lower) * Math.random() + lower;
+    return(higher - lower) * Math.random() + lower;
 }
 
 function $id(id) {
-	return document.getElementById(id);
+    return document.getElementById(id);
 }
 
-function playSnow(){
+function playSnow() {
     test.resume();
 }
 
-function pauseSnow(){
+function pauseSnow() {
+    console.log("pppp")
     test.pause();
-    
+
 }
 
-var pic=new Image();
+function addStar(x, y) {
+    test.sprites.push( createStar(x,y) );
+    //test.context.drawImage(pic, x, y);
+}
+
+var pic = new Image();
 
 
-function loadResource(){
-    pic.src="pic.png";
-    pic.onload=function(){
+function loadResource() {
+    pic.src = "pic.png";
+    pic.onload = function() {
         init();
         test.start();
     }
